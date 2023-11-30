9c39f763fd5b74e33a46ac1ca31951e1bdf28098
woosung <jwos0705@gmail.com>
2023-12-01 00:14:57 +0900

    simple

--- detecter/urls.py
+++ detecter/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
     path('upload/', views.upload, name='upload'),
     path('upload/result/', views.result, name='result'),
 
